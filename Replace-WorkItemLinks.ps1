de3370457ece40608108a86764101414ed64bf97
Vinny Martins <martins.vds@outlook.com>
2022-06-02 13:42:08 -0600

    change typename check

--- Replace-WorkItemLinks.ps1
+++ Replace-WorkItemLinks.ps1
@@ -67,7 +67,7 @@ function ReplaceLinks ($workItemRelation, [object] $mapping) {
         if ($_.ErrorDetails){
             $exceptionDetails = $_.ErrorDetails.Message | ConvertFrom-Json
             
-            if($exceptionDetails.typeName -eq "Microsoft.TeamFoundation.WorkItemTracking.Server.WorkItemLinksLimitExceededException"){
+            if($exceptionDetails.typeName -contains "WorkItemLinksLimitExceededException"){
                 Write-Host "    Failed to add link. Reason: work item with id '$($workItemRelation.target.id)' will exceed the 1000 link limit." -ForegroundColor Red
                 return $false
             }else{
