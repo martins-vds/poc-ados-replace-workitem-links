790e5758556addc48a28cf5b2bda39b0cbd6b329
Vinny Martins <martins.vds@outlook.com>
2022-06-01 21:44:43 -0600

    refactor code

--- Replace-WorkItemLinks.ps1
+++ Replace-WorkItemLinks.ps1
@@ -15,23 +15,33 @@ $ErrorActionPreference = "Stop"
 
 function FormatQuery([string] $projectName, [object] $mapping){
     $queryFormat = "SELECT [System.Id] FROM workitemLinks WHERE ( [Source].[System.TeamProject] = '{0}' AND [Source].[System.WorkItemType] <> '' AND [Source].[System.State] <> '' ) AND ( {1} ) AND ( [Target].[System.TeamProject] = '{0}' AND [Target].[System.WorkItemType] <> '' ) ORDER BY [System.Id] MODE (MustContain)"
-    $queryFilter = $mapping | % {
+    $queryFilter = $mapping | ForEach-Object {
         "[System.Links.LinkType] = '{0}'" -f $_.oldLinkType
     } | Join-String -Separator " OR "
 
     return $queryFormat -f $projectName, $queryFilter
 }
 
-function ReplaceLinks ($workItemRelation, [object] $mapping){
-    Write-Host "Fetching work item with id '$($workItemRelation.source.id)'..." -ForegroundColor White
-
+function ReplaceLinks ($workItemRelation, [object] $mapping){    
     $sourceWorkItem = Invoke-RestMethod -Uri "$($workItemApi -f $workItemRelation.source.id)&`$expand=All" -Method Get -Headers $authenicationHeader -ContentType "application/json"
     
+    $i = 0
+    $found = $false
     $relationCount = $sourceWorkItem.relations.Length
-    for ($i = 0; $i -lt $relationCount; $i++) {
+
+    while ($i -lt $relationCount -and $found -eq $false) {
         if ($sourceWorkItem.relations[$i].rel -eq $mapping.oldLinkType){
-            break;
+            $found = $true
         }
+        
+        if(!$found){
+            $i++
+        }
+    }
+
+    if (!$found){
+        Write-Host "    Link already replaced." -ForegroundColor Yellow
+        return
     }
 
     $removeLinkOperation = @(@{
@@ -39,9 +49,9 @@ function ReplaceLinks ($workItemRelation, [object] $mapping){
         "path" = "/relations/{0}" -f $i
     }) | ConvertTo-Json -AsArray
     
-    Invoke-RestMethod -Uri "$($workItemApi -f $workItemRelation.source.id)" -Method Patch -Body $removeLinkOperation -Headers $authenicationHeader -ContentType "application/json-patch+json" | Out-Null
+    $null = Invoke-RestMethod -Uri "$($workItemApi -f $workItemRelation.source.id)" -Method Patch -Body $removeLinkOperation -Headers $authenicationHeader -ContentType "application/json-patch+json"
 
-    Write-Host "    Removed link type '$($mapping.oldLinkType)'" -ForegroundColor Yellow
+    Write-Host "    Removed link type '$($mapping.oldLinkType)'" -ForegroundColor White
 
     $addLinkOperation = @(
         @{
@@ -66,14 +76,18 @@ function ReplaceLinks ($workItemRelation, [object] $mapping){
         }
     }
 
-    Invoke-RestMethod -Uri "$($workItemApi -f $workItemRelation.source.id)" -Method Patch -Body $($addLinkOperation | ConvertTo-Json -Depth 3 -AsArray) -Headers $authenicationHeader -ContentType "application/json-patch+json" | Out-Null
+    $null = Invoke-RestMethod -Uri "$($workItemApi -f $workItemRelation.source.id)" -Method Patch -Body $($addLinkOperation | ConvertTo-Json -Depth 3 -AsArray) -Headers $authenicationHeader -ContentType "application/json-patch+json"
 
-    Write-Host "    Added link '$($mapping.newLinkType)'" -ForegroundColor Yellow
+    Write-Host "    Added link '$($mapping.newLinkType)'" -ForegroundColor White
+    
+    if ($mapping.tags) {
+        Write-Host "    Added tags '$($mapping.tags)'" -ForegroundColor White
+    }
 }
 
 $authenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($Token)")) }
 
-$wiqlApi = "$CollectionUrl/_apis/wit/wiql?api-version=6.0"
+$wiqlApi = "$CollectionUrl/_apis/wit/wiql?api-version=6.0&`$top={0}&`$skip={1}"
 $workItemApi = "$CollectionUrl/_apis/wit/workitems/{0}?api-version=6.0"
 
 $linkReplacementMapping = @(
@@ -90,23 +104,32 @@ $wiql = @{
 
 Write-Host "Fetching work items with matching links..." -ForegroundColor Cyan
 
-$response = Invoke-RestMethod -Uri $wiqlApi -Method Post -Headers $authenicationHeader -Body $wiql -ContentType "application/json"
-
-if($response.workItemRelations.Length -gt 0){
-
-    Write-Host "Found $($response.workItemRelations.Length) work items." -ForegroundColor Green
-
-    foreach ($relation in $response.workItemRelations) {
-        $mapping = $linkReplacementMapping | where {$_.oldLinkType -eq $relation.rel}
-        
-        if($mapping){
-            ReplaceLinks -workItemRelation $relation -mapping $mapping
+$top = 100
+$skip = 0
+do {    
+    $response = Invoke-RestMethod -Uri $($wiqlApi -f $top, $skip) -Method Post -Headers $authenicationHeader -Body $wiql -ContentType "application/json"
+    
+    if($response.workItemRelations.Length -gt 0){
+        foreach ($relation in $response.workItemRelations) {
+            if($relation.rel){
+                $mapping = $linkReplacementMapping | Where-Object {$_.oldLinkType -eq $relation.rel}
+            
+                if($mapping){
+                    Write-Host "Replacing links on work item with id '$($relation.source.id)'..." -ForegroundColor White
+                    ReplaceLinks -workItemRelation $relation -mapping $mapping
+                }else{
+                    Write-Host "Skipped work item with id '$($relation.source.id)'. No mapping found for link type '$($relation.rel)'" -ForegroundColor Yellow
+                }
+            }
         }
+    
+    }else{
+        Write-Host "No work items found." -ForegroundColor Yellow    
     }
 
-    Write-Host "Done." -ForegroundColor Green
-}else{
-    Write-Host "No work items found." -ForegroundColor Red
-    Write-Host "Done." -ForegroundColor Red
-}
+    $skip += $top
+} until (
+    $response.workItemRelations.Length -le 0
+)
 
+Write-Host "Done." -ForegroundColor Green
