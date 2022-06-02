38c410c02d03294392bf80a6e74d41e8d2cdfeac
Vinny Martins <martins.vds@outlook.com>
2022-06-02 13:31:23 -0600

    add return false

--- Replace-WorkItemLinks.ps1
+++ Replace-WorkItemLinks.ps1
@@ -69,6 +69,7 @@ function ReplaceLinks ($workItemRelation, [object] $mapping) {
             
             if($exceptionDetails.typeName -eq "Microsoft.TeamFoundation.WorkItemTracking.Server.WorkItemLinksLimitExceededException"){
                 Write-Host "    Failed to add link. Reason: work item with id '$($workItemRelation.target.id)' will exceed the 1000 link limit." -ForegroundColor Red
+                return $false
             }else{
                 throw
             }
